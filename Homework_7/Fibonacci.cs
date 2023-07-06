dd4b0dc85b10b59b5781f3b780446f7a50234e95
MYudina <MYudina@PC_MSK-20470>
2023-07-06 11:24:05 +0300

    Add project files.

---
+++ Homework_7/Fibonacci.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_7
+{
+    internal class Fibonacci
+    {
+        public int СycleFibonacci(int n)
+        {
+            int i = 0;
+            int f1 = 1;
+            int f2 = 1;
+            int fSum = 0;
+            while (i < n - 1)
+            {
+                fSum = f1 + f2;
+                f1 = f2;
+                f2 = fSum;
+                i++;
+            }
+            return f2;
+        }
+
+       
+        public int RecursionFibonacci(int n)
+        {
+             if (n == 1 || n == 2)
+             {
+                 return n;
+             }
+             return RecursionFibonacci(n - 1) + RecursionFibonacci(n - 2);
+        }
+    }
+}
---
+++ Homework_7/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Homework_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fib = 0;
+            int fib2 = 0;
+            Stopwatch StopWatch = new();
+
+            Fibonacci fibonacci = new Fibonacci();
+
+            StopWatch.Start();
+            fib = fibonacci.СycleFibonacci(5);
+            StopWatch.Stop();
+            Console.WriteLine("Значение n-го члена последовательности Фибоначчи = {0}. Время нахождения с использованием цикла = {1}", fib, StopWatch.Elapsed.Nanoseconds);
+            StopWatch.Reset();
+
+            StopWatch.Start();
+            fib2 = fibonacci.RecursionFibonacci(5);
+            StopWatch.Stop();
+            Console.WriteLine("Значение n-го члена последовательности Фибоначчи = {0}. Время нахождения с использованием рекурсии = {1}", fib2, StopWatch.Elapsed.Nanoseconds);
+            StopWatch.Reset();
+            Console.WriteLine("\n");
+
+            StopWatch.Start();
+            fib = fibonacci.СycleFibonacci(10);
+            StopWatch.Stop();
+            Console.WriteLine("Значение n-го члена последовательности Фибоначчи = {0}. Время нахождения с использованием цикла = {1}", fib, StopWatch.Elapsed.Nanoseconds);
+            StopWatch.Reset();
+
+            StopWatch.Start();
+            fib2 = fibonacci.RecursionFibonacci(10);
+            StopWatch.Stop();
+            Console.WriteLine("Значение n-го члена последовательности Фибоначчи = {0}. Время нахождения с использованием рекурсии = {1}", fib2, StopWatch.Elapsed.Nanoseconds);
+            StopWatch.Reset();
+            Console.WriteLine("\n");
+
+            StopWatch.Start();
+            fib = fibonacci.СycleFibonacci(20);
+            StopWatch.Stop();
+            Console.WriteLine("Значение n-го члена последовательности Фибоначчи = {0}. Время нахождения с использованием цикла = {1}", fib, StopWatch.Elapsed.Nanoseconds);
+            StopWatch.Reset();
+
+            StopWatch.Start();
+            fib2 = fibonacci.RecursionFibonacci(20);
+            StopWatch.Stop();
+            Console.WriteLine("Значение n-го члена последовательности Фибоначчи = {0}. Время нахождения с использованием рекурсии = {1}", fib2, StopWatch.Elapsed.Nanoseconds);
+            StopWatch.Reset();
+        }
+    }
+}
